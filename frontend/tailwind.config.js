70de5060a3b8ea04519d8a018b4809ba08b47b19
srames50 <2shyamr18@gmail.com>
2025-02-17 13:16:41 -0800

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,html}', // Modify if necessary to match your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
