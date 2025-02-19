16fd7f88868ee3fd5d53adf0115784490ea5cd59
vikranth <vikmandadi@gmail.com>
2025-02-19 00:21:17 -0600

    connection to database working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,8 @@ import "./index.css";
 function App() {
   return (
     <div>
-      <h1 className="underline text-gray-500 font-bold">Hello world!</h1>
-      {/* Test other colors to see if any work */}
-      <p className="text-blue-500">Blue test</p>
-      <p className="text-red-500">Red test</p>
-      <p className="text-black">Black test</p>
+      <h1 className="text-3xl text-teal-500">Welcome to EMS</h1>
+
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -12,3 +12,4 @@ app.use(express.json());
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
+
