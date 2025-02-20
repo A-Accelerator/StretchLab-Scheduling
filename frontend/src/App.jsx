dc3cecfd2f7a63fc349ae0741cf3bb4fe2ba1dfa
vikranth <vikmandadi@gmail.com>
2025-02-19 22:06:44 -0600

    auth changes and removing env from git tracking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import "./index.css";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard.jsx"
+import EmployeeDashboard from "./pages/EmployeeDashboard.jsx";
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl text-teal-500">Welcome to EMS</h1>
 
-    </div>
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+        <Route path="/employee-dashboard" element={<EmployeeDashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.js';
+import connectToDatabase from './db/db.js';
 
 dotenv.config();
 
+connectToDatabase();
 const app = express();
 app.use(cors())
 app.use(express.json());
+app.use('/api/auth', authRouter);
 
 
 app.listen(process.env.PORT || 3000, () => {
